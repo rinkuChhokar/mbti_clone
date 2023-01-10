d46b1c1521c77d77cf9a09ec53b7af33eb921424
rinkuChhokar <rinksplash007@gmail.com>
2023-01-10 20:15:11 +0530

    updated database file

--- database.py
+++ database.py
@@ -1,18 +1,17 @@
 from deta import Deta
 import os
-from dotenv import load_dotenv
+import streamlit as st
 
 
-load_dotenv(".env")
+# DETA_KEY = st.secrets["dkey"]
 
-DETA_KEY = os.getenv("DETA_KEY")
-
-DETA_KEY2 = os.getenv("DETA_KEY2")
+# DETA_KEY2 = st.secrets["dkey2"]
 
 
-deta = Deta(DETA_KEY)
+deta = Deta(st.secrets["DETA_KEY"])
 
-deta1 = Deta(DETA_KEY2)
+deta1 = Deta(st.secrets["DETA_KEY2"])
+
 
 db = deta.Base("info_db")
 
@@ -21,30 +20,27 @@ usrdb = deta1.Base("login_db")
 
 # Functions related to admin page-
 
-def insert_new_admin(username,name,password):
+def insert_new_admin(username, name, password):
 
-  return usrdb.put({"key":username,"Name":name,"Password":password})
- 
+    return usrdb.put({"key": username, "Name": name, "Password": password})
+
 
 def fetch_admin_details():
 
-  res = usrdb.fetch()
+    res = usrdb.fetch()
 
-  return res.items
+    return res.items
 
 
 # Functions related to home page-
 
-def insert_values(date_t,uid,name,gender,ptype,result):
+def insert_values(date_t, uid, name, gender, ptype, result):
 
-  return db.put({"Date and Time":date_t,"key":uid,"Name":name,"Gender":gender,"Person":ptype,"Personality Type":result})
- 
+    return db.put({"Date and Time": date_t, "key": uid, "Name": name, "Gender": gender, "Person": ptype, "Personality Type": result})
+
 
 def fetch_details():
 
-  res = db.fetch()
+    res = db.fetch()
 
-  return res.items
-
-
-
+    return res.items
